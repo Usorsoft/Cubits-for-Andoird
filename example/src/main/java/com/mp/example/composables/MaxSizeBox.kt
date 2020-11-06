b1a0a144dbca583a066c2a4fe4c6351103de7f9d
Michael Pankraz <>
2020-11-06 20:51:29 +0100

    Introduce custom comosables

---
+++ example/src/main/java/com/mp/example/composables/MaxSizeBox.kt
@@ -0,0 +1,14 @@
+package com.jamitlabs.remoteui_sdk.composables
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MaxSizeBox(alignment: Alignment, content: @Composable () -> Unit) {
+    Box(modifier = Modifier.fillMaxSize(), alignment = alignment) {
+        content()
+    }
+}
